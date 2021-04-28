4bd9c9a43ff3b4539baca1b1650ca7d55f913117
TheMeatball <cedric.vangheluwe@hotmail.com>
2021-04-28 14:19:08 +0200

    creation d'une recherche sur toutes les catzgories différentes et d'un btn 'all'

--- mainApp.js
+++ mainApp.js
@@ -5,10 +5,12 @@ let btnCreaturesFood = document.querySelector('.btnCreaturesFood');
 let btnMaterials = document.querySelector('.btnMaterials');
 let btnMonsters = document.querySelector('.btnMonsters');
 let btnEquipments = document.querySelector('.btnEquipments');
-let btnResearch = document.querySelector('.btnResearch');
+let btnAll = document.querySelector('.btnAll');
 let container = document.querySelector('.container');
 let containerList = document.querySelector('.containerList');
 let formResearch = document.querySelector('.formResearch');
+let formResearchInput = document.querySelector('.formResearchInput');
+let btnSubmit = document.querySelector('.btnSubmit');
 
 let btnsContainer = document.querySelector('.btnsContainer');
 let y = 0;
@@ -34,23 +36,89 @@ function liElement(input){
     li.appendChild(img);
 }
 
-function createList(dataCat, btnClicked){
+function recherche(dataCat){
+    let arrayNames = [];
+    for(let arraysNameEl of dataCat){
+        arrayNames.push(arraysNameEl.name);
+    }
+    arrayNames.sort();
+
+    formResearch.addEventListener('submit', (e) =>{
+        e.preventDefault();
+        let formInput = document.querySelector('#filter-term').value.toLowerCase();
+        // get users that includes input
+        let filteredInput = dataCat.filter((arraysEl) =>{
+            return arraysEl.name.toLowerCase().includes(formInput);
+        });
+        // create li with users informations
+        containerList.innerHTML = "";
+        
+        for(let j = 0 ; j < arrayNames.length ; j++){
+            for(let i = 0 ; i < filteredInput.length ; i++){
+                
+                if( filteredInput[i].name == "molduking"        ||
+                filteredInput[i].name == "monk maz koshia"      ||
+                filteredInput[i].name == "igneo talus titan"    ||
+                filteredInput[i].name == 'ancient battle axe+'  ||
+                filteredInput[i].name == 'ancient battle axe++' ||
+                filteredInput[i].name == 'golden bow'           ||
+                filteredInput[i].name == 'golden claymore'      ||
+                filteredInput[i].name == 'guardian shield+'     ||
+                filteredInput[i].name == 'guardian shield++'    ||
+                filteredInput[i].name == 'guardian spear+'      ||
+                filteredInput[i].name == 'guardian spear++'     ||
+                filteredInput[i].name == 'guardian sword+'      ||
+                filteredInput[i].name == 'guardian sword++'     ||
+                filteredInput[i].name == 'one-hit obliterator'){
+                    // images inexistantes
+                }else 
+                if(filteredInput[i].name == arrayNames[j]){
+                    liElement(filteredInput[i]);
+                }
+            };
+        };
+        formResearchInput.value = "";
+    });
+}
+
+function createList(dataCat, btnClicked, cat){
     btnClicked.removeAttribute('disabled');
-    
+    formResearchInput.removeAttribute('disabled');
+    btnSubmit.removeAttribute('disabled');
+
     btnClicked.addEventListener('click', (e) =>{
-        if(!formResearch.classList.contains('formResearch--hidden')){   
-            formResearch.classList.add('formResearch--hidden');
-        }
+        
         containerList.innerHTML = '';
         let dataCatNames = [];
         for(let dataCatNameEl of dataCat){
             dataCatNames.push(dataCatNameEl.name);
         }
         dataCatNames.sort();
+
+        if(cat == 'creatures'){
+            console.log('creatures');
+            recherche(dataCat);
+        }else if(cat == 'creaturesFood'){
+            console.log('creaturesFood');
+            recherche(dataCat);
+        }else if(cat == 'materials'){
+            console.log('materials');
+            recherche(dataCat);
+        }else if(cat == 'equipment'){
+            console.log('equipment');
+            recherche(dataCat);
+        }else if(cat == 'monsters'){
+            console.log('monsters');
+            recherche(dataCat);
+        }else if(cat == 'all'){
+            console.log('all');
+            recherche(dataCat);
+        }
+
         for(let j = 0 ; j < dataCatNames.length ; j++){
             for(let i = 0 ; i < dataCat.length ; i++){
                 
-                if( dataCat[i].name == "molduking"            ||
+                if( dataCat[i].name == "molduking"        ||
                 dataCat[i].name == "monk maz koshia"      ||
                 dataCat[i].name == "igneo talus titan"    ||
                 dataCat[i].name == 'ancient battle axe+'  ||
@@ -75,6 +143,7 @@ function createList(dataCat, btnClicked){
     });
 }
 
+
 fetch('https://botw-compendium.herokuapp.com/api/v2')
 .then((response) =>{
     return response.json();
@@ -92,61 +161,20 @@ fetch('https://botw-compendium.herokuapp.com/api/v2')
     arrayConcat = arrayConcat.concat(database.data.monsters);
     const allArrays = arrayConcat.concat(database.data.equipment);
 
-    createList(creatures, btnCreatures);
-    createList(creaturesFood, btnCreaturesFood);
-    createList(materials, btnMaterials);
-    createList(equipment, btnEquipments);
-    createList(monsters, btnMonsters);
+    let catAll = 'all';
+    let cat1 = 'creatures';
+    let cat2 = 'creaturesFood';
+    let cat3 = 'materials';
+    let cat4 = 'equipment';
+    let cat5 = 'monsters';
+    createList(allArrays, btnAll, catAll);
+    createList(creatures, btnCreatures, cat1);
+    createList(creaturesFood, btnCreaturesFood, cat2);
+    createList(materials, btnMaterials, cat3);
+    createList(equipment, btnEquipments, cat4);
+    createList(monsters, btnMonsters, cat5);
 
     
-    // recherche dans toutes les catégories
-    btnResearch.removeAttribute('disabled');
-    btnResearch.addEventListener('click', (e) =>{
-        formResearch.classList.toggle('formResearch--hidden');
-        containerList.innerHTML = '';
-        
-        let allArraysNames = [];
-        for(let allArraysNameEl of allArrays){
-            allArraysNames.push(allArraysNameEl.name);
-        }
-        allArraysNames.sort();
-
-        formResearch.addEventListener('submit', (e) =>{
-            e.preventDefault();
-            let formInput = document.querySelector('#filter-term').value.toLowerCase();
-            // get users that includes input
-            let filteredInput = allArrays.filter((allArraysEl) =>{
-                return allArraysEl.name.toLowerCase().includes(formInput);
-            });
-            // create li with users informations
-            containerList.innerHTML = "";
-            
-            for(let j = 0 ; j < allArraysNames.length ; j++){
-                for(let i = 0 ; i < filteredInput.length ; i++){
-                    
-                    if( filteredInput[i].name == "molduking"            ||
-                    filteredInput[i].name == "monk maz koshia"      ||
-                    filteredInput[i].name == "igneo talus titan"    ||
-                    filteredInput[i].name == 'ancient battle axe+'  ||
-                    filteredInput[i].name == 'ancient battle axe++' ||
-                    filteredInput[i].name == 'golden bow'           ||
-                    filteredInput[i].name == 'golden claymore'      ||
-                    filteredInput[i].name == 'guardian shield+'     ||
-                    filteredInput[i].name == 'guardian shield++'    ||
-                    filteredInput[i].name == 'guardian spear+'      ||
-                    filteredInput[i].name == 'guardian spear++'     ||
-                    filteredInput[i].name == 'guardian sword+'      ||
-                    filteredInput[i].name == 'guardian sword++'     ||
-                    filteredInput[i].name == 'one-hit obliterator'){
-                        // images inexistantes
-                    }else 
-                    if(filteredInput[i].name == allArraysNames[j]){
-                        liElement(filteredInput[i]);
-                    }
-                };
-            };
-        });
-    });
 })
 .catch((response) =>{
 console.log('data non trouvée => ' + response);
